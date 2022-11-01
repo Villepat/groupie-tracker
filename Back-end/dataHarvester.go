06777492362f41aa72101451e5de4ff342892eba
villepat <ville.patjas@hotmail.fi>
2022-11-01 12:17:44 +0200

    updates

--- Back-end/dataHarvester.go
+++ Back-end/dataHarvester.go
@@ -1,6 +1,22 @@
 package server
 
+
+
 func main (){
+
+	type Artist struct {
+		Id           int      `json:"id"`
+		Image        string   `json:"image"`
+		Name         string   `json:"name"`
+		Members      []string `json:"members"`
+		CreationDate int      `json:"creationDate"`
+		FirstAlbum   string   `json:"firstAlbum"`
+		Locations    string   `json:"locations"`
+		ConcertDates string   `json:"concertDates"`
+		Relations    string   `json:"relations"`
+		Dates        string   `json:"dates"`
+	}
+	
 	//Gets data from URL and returns the body of data as []byte and an error
 	func harvest(url string) []byte, error {
 		res, err := http.Get(url) //get contents of API
