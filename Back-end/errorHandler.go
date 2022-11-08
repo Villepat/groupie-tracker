0ca7fb4c704a1879af77a62ca850c33999087f94
crippe-b@hotmail.com <crippe-b@hotmail.com>
2022-11-08 17:12:45 +0200

    removed commented code

--- Back-end/errorHandler.go
+++ Back-end/errorHandler.go
@@ -21,14 +21,12 @@ func error400(w http.ResponseWriter) {
 	w.WriteHeader(400)
 	Err.ErrorMessage = "Bad request"
 	Err.ErrorNum = 400
-	//w.Write([]byte("400 Bad request")
 	errorPage(w)
 }
 func error404(w http.ResponseWriter) {
 	w.WriteHeader(404)
 	Err.ErrorMessage = "Page not found"
 	Err.ErrorNum = 404
-	//w.Write([]byte("404 Page not found"))
 	errorPage(w)
 }
 func error500(w http.ResponseWriter) {
@@ -36,5 +34,4 @@ func error500(w http.ResponseWriter) {
 	Err.ErrorMessage = "Internal Server Error"
 	Err.ErrorNum = 500
 	errorPage(w)
-	//w.Write([]byte("500 Internal Server Error"))
 }
